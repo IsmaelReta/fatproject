180981fc636c2c335ac2a80e7cce6bd6c93cb739
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-06-30 10:23:25 -0300

    something

--- data/views.py
+++ data/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from data.models import HealthInsurance, Product, Province, Address, User, Order, OrderDetail
 
 # Create your views here.
+
 def home(request):
     return render(request, 'homev2.html')
-
+'''
+def reacthome(request):
+    return render(request, 'react_things/public/index.html')
+'''
 def form1(request):
     return render(request, 'form1.html')
--- settings/urls.py
+++ settings/urls.py
@@ -20,5 +20,6 @@ from data import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    #path('', views.reacthome, name="reacthome"),
     path('form1/', views.form1, name="form1")
 ]
