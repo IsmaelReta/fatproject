c6d7d9586f52a4010db7fbccf23923b2a1f97bb3
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-08-16 11:13:57 -0300

    fixed some paths

--- django_fqc/config/settings.py
+++ django_fqc/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_fqc.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_fqc.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
