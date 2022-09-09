ac03ea002e1ce789ad0d7ad046606308df9cb643
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-09 10:36:59 -0300

    new apps

---
+++ django_fqc/disability_certificate/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DisabilityCertificateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'disability_certificate'
---
+++ django_fqc/health_insurance/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HealthInsuranceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'health_insurance'
