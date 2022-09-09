142bf673c305063e8f3b21adbca5dce9bcdbfc3a
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-09 12:10:13 -0300

    apps structure changes

--- django_fqc/employee/apps.py
+++ django_fqc/employee/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TutorConfig(AppConfig):
+class EmployeeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tutor'
+    name = 'employee'
