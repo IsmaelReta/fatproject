e5e2b06a33fa0a22b9355fd37f29142093c9dba2
MatiasCarbel <matiasjcarbel@gmail.com>
2022-09-13 08:28:27 -0300

    last commit

--- django_fqc/employee/serializers.py
+++ django_fqc/employee/serializers.py
@@ -1,6 +1,4 @@
-from pyexpat import model
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from .models import Employee
 
 class EmployeeSerializer(serializers.Serializer):
