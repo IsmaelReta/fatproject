7eb3f470408a7089e976e863f3b3073ee69b1524
MatiasCarbel <matiasjcarbel@gmail.com>
2022-09-13 08:38:37 -0300

    views and serializers sale

--- django_fqc/sale/views.py
+++ django_fqc/sale/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
+from .models import Sale, SaleDetail
+from .serializers import SaleSerializer,SaleDetailSerializer
 # Create your views here.
+
+class SaleViewSet(viewsets.ModelViewSet):
+    serializer_class = SaleSerializer
+
+    def get_queryset(self):
+        sale = Sale.objects.all()
+        return sale
+
+
+class SaleDetailViewSet(viewsets.ModelViewSet):
+    serializer_class = SaleDetailSerializer
+
+    def get_queryset(self):
+        sale_detail = SaleDetail.objects.all()
+        return sale_detail
+    
