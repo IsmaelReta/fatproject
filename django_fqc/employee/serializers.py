24ef42a8e092151b972e1114574b5a194950def2
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-14 11:00:32 -0300

    minor changes

--- django_fqc/employee/serializers.py
+++ django_fqc/employee/serializers.py
@@ -3,10 +3,12 @@ from pyexpat import model
 from rest_framework import serializers
 from .models import Employee, User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
 
 class EmployeeSerializer(serializers.ModelSerializer):
     user = UserSerializer()
