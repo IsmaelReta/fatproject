1a498d76ca8412660d91bc932ad0e48ca4ed6ed4
MatiasCarbel <matiasjcarbel@gmail.com>
2022-09-14 10:22:24 -0300

    employee serializer changed

--- django_fqc/employee/serializers.py
+++ django_fqc/employee/serializers.py
@@ -1,9 +1,16 @@
+from dataclasses import fields
+from pyexpat import model
 from rest_framework import serializers
-from .models import Employee
+from .models import Employee, User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 class EmployeeSerializer(serializers.ModelSerializer):
-    
+    user = UserSerializer()
+
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = ['id', 'user']
