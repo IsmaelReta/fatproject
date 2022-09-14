dd6e9e8e8b82778292e65246870fb6e2622ee5ec
MatiasCarbel <matiasjcarbel@gmail.com>
2022-09-14 12:11:00 -0300

    patient router name

--- django_fqc/patient/routers.py
+++ django_fqc/patient/routers.py
@@ -3,7 +3,7 @@ from .views import PatientViewSet, TutorViewSet, CertificateViewSet, HealthInsur
 
 router = DefaultRouter()
 
-router.register('patients', PatientViewSet, basename='patients')
+router.register('', PatientViewSet, basename='patients')
 router.register('tutors', TutorViewSet, basename='tutors')
 router.register('certificates', CertificateViewSet, basename='certificates')
 router.register('healths', HealthInsuranceViewSet, basename='healths')
