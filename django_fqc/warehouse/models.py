c7b1365e645383d0c54f17748987038eb409223a
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-20 12:18:18 -0300

    serializer

--- django_fqc/warehouse/models.py
+++ django_fqc/warehouse/models.py
@@ -14,7 +14,7 @@ class Inventory(models.Model):
     quantity = models.PositiveIntegerField()
     expiration_date = models.DateField()
     type = models.CharField(max_length=255)
-    warehouse = models.ForeignKey('warehouse.Warehouse', on_delete=models.DO_NOTHING)
+    warehouse = models.ForeignKey('warehouse.Warehouse', related_name='inventory', on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
         return f'{self.product} - ID:{self.id}'
--- django_fqc/warehouse/serializers.py
+++ django_fqc/warehouse/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from .models import Warehouse, Inventory
 
 
-class WarehouseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Warehouse
-        fields = '__all__'
-
-
 class InventorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Inventory
         fields = '__all__'
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    inventory = InventorySerializer(many=True)
+
+    class Meta:
+        model = Warehouse
+        # fields = '__all__'
+        fields = ['name', 'inventory']
