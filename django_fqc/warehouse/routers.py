4e62d807d307055df55b15cede6ba4bd98e86e22
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-20 12:08:43 -0300

    warehouse router update

--- django_fqc/warehouse/routers.py
+++ django_fqc/warehouse/routers.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from warehouse.views import WarehouseViewSet, InventoryViewSet # noqa
 
 router = routers.DefaultRouter()
-router.register('warehouses', WarehouseViewSet, basename='warehouses')
+router.register('', WarehouseViewSet, basename='warehouses')
 router.register('inventories', InventoryViewSet, basename='inventories')
 
 urlpatterns = router.urls
