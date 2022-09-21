2cbc96fb379cae2b987224785b7d51f5e5d5b8ea
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-21 11:33:41 -0300

    settings & models update

--- django_fqc/patient/models.py
+++ django_fqc/patient/models.py
@@ -7,6 +7,8 @@ class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     document_number = models.CharField(max_length=8)
     birth_date = models.DateField(null=True)
+    province = models.CharField(max_length=25)
+    city = models.CharField(max_length=30)
 
     def __str__(self) -> str:
         return f'{self.user} - Patient_ID:{self.id}'
