cffec57a93f892f14f46d2a0ffd48ec8f8c6ff48
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-21 09:43:42 -0300

    settings & views

--- django_fqc/product/views.py
+++ django_fqc/product/views.py
@@ -1,14 +1,27 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
+from warehouse.models import Inventory
+from warehouse.serializers import InventorySerializer
 from .serializers import ProductSerializer
+from django.db.models import lookups
 
 # Create your views here.
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    serializer_class = ProductSerializer
+    serializer_class = InventorySerializer
+    queryset = Inventory.objects
 
+    def filter_queryset(self, queryset):
+        now_time_plus = datetime.now() + timedelta(days=30)
+        print("time plus", now_time_plus)
+        inventory = Inventory.objects.filter(expiration_date__gte=now_time_plus)
+        return inventory
+
+    """
     def get_queryset(self):
         product = Product.objects.all()
         return product
+    """
--- django_fqc/warehouse/serializers.py
+++ django_fqc/warehouse/serializers.py
@@ -1,18 +1,18 @@
-# from rest_framework import serializers
-# from .models import Warehouse, Inventory
-#
-#
-# class InventorySerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Inventory
-#         fields = '__all__'
-#
-#
-# class WarehouseSerializer(serializers.ModelSerializer):
-#     inventory = InventorySerializer(many=True)
-#
-#     class Meta:
-#         model = Warehouse
-#         # fields = '__all__'
-#         fields = ['name', 'inventory']
+from rest_framework import serializers
+from .models import Warehouse, Inventory
+
+
+class InventorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    inventory = InventorySerializer(many=True)
+
+    class Meta:
+        model = Warehouse
+        # fields = '__all__'
+        fields = ['name', 'inventory']
