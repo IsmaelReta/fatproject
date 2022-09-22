ae500ccf6c434aed7f1c2419b30c4fdaa046e218
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-22 09:18:39 -0300

    slack test 4

--- django_fqc/config/urls.py
+++ django_fqc/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# slack test 3
+# slack test 4
 
 urlpatterns = [
     path('', admin.site.urls),
