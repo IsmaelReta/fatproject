d7b785bb79a36f13c9721a3223d117144f502f8d
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-28 10:16:54 -0300

    django admin language update

--- django_fqc/config/settings.py
+++ django_fqc/config/settings.py
@@ -156,7 +156,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
 TIME_ZONE = 'UTC'
 
