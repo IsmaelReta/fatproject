d5f12a49a13697dd6496ce74ce82e64be5c2fee0
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-09-30 11:43:56 -0300

    router patients update

--- django_fqc/patient/routers.py
+++ django_fqc/patient/routers.py
@@ -4,7 +4,7 @@ from .views import PatientViewSet, PatientTutorViewSet, PatientCertificateViewSe
 
 router = DefaultRouter()
 
-router.register('', PatientViewSet, basename='patients')
+router.register('list', PatientViewSet, basename='patients')
 router.register(r'(?P<patient_id>[^/.]+)/tutors', PatientTutorViewSet, basename='tutors')
 router.register(r'(?P<patient_id>[^/.]+)/certificates', PatientCertificateViewSet, basename='certificates')
 router.register(r'(?P<patient_id>[^/.]+)/healthinsurances', PatientHealthInsViewSet, basename='healthsinsurances')
