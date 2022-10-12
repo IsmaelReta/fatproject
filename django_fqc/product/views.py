f4226803472bf3a335cc70b6f2ec528a417874ce
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-10-12 14:29:22 -0300

    product view update

--- django_fqc/product/views.py
+++ django_fqc/product/views.py
@@ -14,7 +14,8 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects
 
     def filter_queryset(self, queryset):
-        now_time_plus = datetime.now() + timedelta(days=30)
-        products_date_filtered = Product.objects.filter(inventory__expiration_date__gte=now_time_plus)
-        products_with_quantity = products_date_filtered.annotate(total_products=Sum('inventory__quantity'))
+        #now_time_plus = datetime.now() + timedelta(days=30)
+        #products_date_filtered = Product.objects.filter(inventory__expiration_date__gte=now_time_plus)
+        # products_with_quantity = products_date_filtered.annotate(total_products=Sum('inventory__quantity'))
+        products_with_quantity = Product.objects.annotate(total_products=Sum('inventory__quantity'))
         return products_with_quantity
