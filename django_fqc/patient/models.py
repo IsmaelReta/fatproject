39f11f091e67ec9a4782d592c762b911836ac2e3
MatiasCarbel <matiasjcarbel@gmail.com>
2022-10-27 10:20:55 -0300

    base64 fixed?

--- django_fqc/patient/models.py
+++ django_fqc/patient/models.py
@@ -34,9 +34,8 @@ class Certificate(models.Model):
 
     def image(self):
         return format_html(
-            f'<a href="data:image/jpeg;base64,{base64.b64encode(self.image_binary + "=" * (-len(self.image_binary) % 4)).decode()}" target="_blank">'
-            f'<img src="data:image/jpeg;base64,{base64.b64encode(self.image_binary + "=" * (-len(self.image_binary) % 4)).decode()}"'
-            # b64decode(data + "=" * (-len(data) % 4))
+            f'<a href="data:image/jpeg;base64,{base64.b64encode(self.image_binary).decode()}" target="_blank">'
+            f'<img src="data:image/jpeg;base64,{base64.b64encode(self.image_binary).decode()}"'
             f' width="100px" height="50px" class="img-thumbnail"> </a>'
         )
 
