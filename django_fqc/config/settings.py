0ea54965bfa10abd25c2aec0133ebb8a32fd9597
MatiasCarbel <matiasjcarbel@gmail.com>
2022-10-28 09:21:51 -0300

    jwt settings

--- django_fqc/config/settings.py
+++ django_fqc/config/settings.py
@@ -125,6 +125,7 @@ REST_FRAMEWORK = {
     # ),
 
     'DEFAULT_PERMISSION_CLASSES': (
+        # 'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticated',
     ),
 
