ba5c18a80522559ba1669027fb7329debf890f8b
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-10-28 08:30:44 -0300

    language

--- django_fqc/config/settings.py
+++ django_fqc/config/settings.py
@@ -233,7 +233,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-AR'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'America/Argentina/Cordoba'
 
