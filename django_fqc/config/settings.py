a780b9156eb97eca3f721f0856a7e9aab7454452
MatiasCarbel <matiasjcarbel@gmail.com>
2022-10-31 11:34:12 -0300

    commit

--- django_fqc/config/settings.py
+++ django_fqc/config/settings.py
@@ -101,7 +101,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 AUTHENTICATION_BACKENDS = [
     # allauth specific authentication methods, such as login by e-mail
