1d654d39ac9aacb22eb3d4802bbe3b0eca747d9d
MatiasCarbel <matiasjcarbel@gmail.com>
2022-11-03 10:25:13 -0300

    HealthInsuranceViewSet mixins

--- django_fqc/healthinsurance/views.py
+++ django_fqc/healthinsurance/views.py
@@ -2,14 +2,17 @@ from pyexpat import model
 from django.shortcuts import render
 from .models import HealthInsurance
 from .serializers import HealthInsuranceSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status, mixins
+from rest_framework.response import Response
 
 # Create your views here.
 
-class HealthInsuranceViewSet(viewsets.ModelViewSet):
+
+class HealthInsuranceViewSet(mixins.ListModelMixin,
+                             mixins.RetrieveModelMixin,
+                             viewsets.GenericViewSet):
     serializer_class = HealthInsuranceSerializer
 
     def get_queryset(self):
         insurance = HealthInsurance.objects.all()
         return insurance
-        
