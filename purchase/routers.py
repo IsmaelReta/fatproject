27bcc28a5ac1ed688502a9293bebcfe0ae2afdd3
Ismael <53263914+IsmaelReta@users.noreply.github.com>
2022-11-17 11:39:47 -0300

    deleted purchase app/model

--- purchase/routers.py
+++
@@ -1,8 +0,0 @@
-# from rest_framework import routers
-# from purchase.views import PurchaseViewSet, PurchaseDetailViewSet # noqa
-#
-# router = routers.DefaultRouter()
-# router.register('', PurchaseViewSet, basename='Purchases')
-# router.register(r'(?P<purchase_id>[^/.]+)/details', PurchaseDetailViewSet, basename='PurchaseDetails')
-#
-# urlpatterns = router.urls
