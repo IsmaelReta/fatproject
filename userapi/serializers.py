221ed9b675cf960dbfd3cb0af611234ebe80bd72
MatiasCarbel <matiasjcarbel@gmail.com>
2022-11-17 11:22:21 -0300

    comments deleted

--- userapi/serializers.py
+++ userapi/serializers.py
@@ -19,23 +19,3 @@ class UserFullSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'patient']
 
-        # def update(self, instance, validated_data):
-        #         patient = validated_data.get('patient')
-        #         instance.patient.document_number = patient.get('document_number')
-        #         instance.patient.document_number = patient.get('document_number')
-        #         instance.patient.document_number = patient.get('document_number')
-        #         instance.patient.document_number = patient.get('document_number')
-        #         instance.patient.document_number = patient.get('document_number')
-        #         instance.patient.document_number = patient.get('document_number')
-        #         instance.patient.document_number = patient.get('document_number')
-        #         instance.user.save()
-
-        #         print("hey")
-        #         return instance
-
-#     def update(self, instance, validated_data):
-#         health_insurance = validated_data.pop('health_insurance')
-#         instance.first_name = validated_data.get('first_name', instance.first_name)
-#         instance.last_name = validated_data.get('last_name', instance.last_name)
-
-#         return instance
