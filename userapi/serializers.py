49107c41984c80d78f0c19634276c9c460df1fca
MatiasCarbel <matiasjcarbel@gmail.com>
2022-11-17 10:14:25 -0300

    nested changes

--- userapi/serializers.py
+++ userapi/serializers.py
@@ -9,36 +9,33 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+
+
+
 class UserFullSerializer(serializers.ModelSerializer):
-    health_insurance = serializers.SerializerMethodField()
-    certificate = serializers.SerializerMethodField()
-    tutor = serializers.SerializerMethodField()
     patient = PatientSerializer()
-
-
-
-    def get_health_insurance(self, obj):
-            pat = Patient.objects.filter(user=obj)
-            customer_account_query = HealthInsurancePatient.objects.filter(patient__in=pat)
-            serializer = HealthInsurancePatientSerializer(customer_account_query, many=True)
-    
-            return serializer.data
-
-    def get_certificate(self, obj):
-            pat = Patient.objects.filter(user=obj)
-            customer_account_query = Certificate.objects.filter(patient__in=pat)
-            serializer = CertificateSerializer(customer_account_query, many=True)
-    
-            return serializer.data
-
-    def get_tutor(self, obj):
-            pat = Patient.objects.filter(user=obj)
-            customer_account_query = Tutor.objects.filter(patient__in=pat)
-            serializer = TutorSerializer(customer_account_query, many=True)
-    
-            return serializer.data
-
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'patient', 'tutor', 'certificate', 'health_insurance']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'patient']
+
+        # def update(self, instance, validated_data):
+        #         patient = validated_data.get('patient')
+        #         instance.patient.document_number = patient.get('document_number')
+        #         instance.patient.document_number = patient.get('document_number')
+        #         instance.patient.document_number = patient.get('document_number')
+        #         instance.patient.document_number = patient.get('document_number')
+        #         instance.patient.document_number = patient.get('document_number')
+        #         instance.patient.document_number = patient.get('document_number')
+        #         instance.patient.document_number = patient.get('document_number')
+        #         instance.user.save()
+
+        #         print("hey")
+        #         return instance
+
+#     def update(self, instance, validated_data):
+#         health_insurance = validated_data.pop('health_insurance')
+#         instance.first_name = validated_data.get('first_name', instance.first_name)
+#         instance.last_name = validated_data.get('last_name', instance.last_name)
+
+#         return instance
